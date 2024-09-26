a33c5941cb5d6e03a6edb752e629715dac234e81
Draweshwarkumar <shivamjha22948@gmail.com>
2024-09-26 19:54:33 +0530

    .

--- src/main/java/com/email/controller/EmailController.java
+++ src/main/java/com/email/controller/EmailController.java
@@ -3,15 +3,18 @@ package com.email.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.email.model.EmailRequest;
+import com.email.model.EmailResponse;
 import com.email.service.EmailService;
 
 @RestController
+@CrossOrigin
 public class EmailController
 
 {
@@ -30,12 +33,17 @@ public class EmailController
 		System.out.println(request);
 		boolean result = this.emailservice.sendEmail(request.getSubject(), request.getMessage(), request.getTo());
 		if(result) {
-			return ResponseEntity.ok("Email is sent successfully....");
+			return ResponseEntity.ok(new EmailResponse("Email is sent successfully..."));
 		}
 		else {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Email not sent....");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new EmailResponse("Email not sent...."));
 		}
 		
+	}
+
+	private String token(String string) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
